0a5234ed2b54a574d68412a52600846cd8082091
GenkoPuhov0220 <genkopuhov2002@gmail.com>
2026-02-06 15:35:58 +0200

    Add Inventory Controller

---
+++ InventoryApp.Web/Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using InventoryApp.Data;
+using InventoryApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApp.Web.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private InventoryDbContext dbContext;
+
+        public InventoryController(InventoryDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Inventory> allInventories = dbContext
+                .Inventories
+                .ToList();
+
+            return View(allInventories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Inventory inventory)
+        {
+            
+            dbContext.Inventories.Add(inventory);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
